83df580fea309e997e34a289b989ff7f68d6f10c
Lefrec <blackfox244@gmail.com>
2024-11-19 16:33:47 +0100

    fix scrollAmount

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,18 @@ const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 const premierItem = document.querySelector(".carousel__item");
+//scrollAmount calculé que au chargement de la page donc préférable de rappeller clientWidth à chaque fois
 const scrollAmount = premierItem.clientWidth;
 
 // Largeur de défilement d’un item
 if (carousel) {
     // Scroll au clic sur le bouton précédent
     prevButton.addEventListener("click", (event) => {
-        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+        carousel.scrollBy({ left: -premierItem.clientWidth, behavior: "smooth" });
     });
 
     // Scroll au clic sur le bouton suivant
     nextButton.addEventListener("click", (event) => {
-        carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+        carousel.scrollBy({ left: +premierItem.clientWidth, behavior: "smooth" });
     });
 }
